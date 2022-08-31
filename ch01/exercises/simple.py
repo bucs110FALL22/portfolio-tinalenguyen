7c57ba008868786dacec39a9d938998822e2314d
Tina Nguyen <72938484+tinalenguyen@users.noreply.github.com>
2022-08-31 20:47:35 +0000

    added ch1

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,21 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+# the last answer is not rounded at the end correctly
+
+rate = int(input("What is the current exchange rate for Euros to Dollars?"))
+
+amount = int(input("What is the amount of currency you'll be exchanging?"))
+
+total = amount/rate
+result = total - 3
+
+print(result)
+
