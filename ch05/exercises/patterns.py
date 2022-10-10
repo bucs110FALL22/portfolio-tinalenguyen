13e4f6cb37e04f7ccf44652049f5c9dde0275643
Tina Nguyen <72938484+tinalenguyen@users.noreply.github.com>
2022-10-10 20:25:15 +0000

    did patterns exercise

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,14 @@
+
+def rstar_pyramid():
+  rows = int(input("How many rows?"))
+  while rows >=0:
+    print(rows*"*")
+    rows-=1
+    
+def star_pyramid():
+  rows = int(input("How many rows?"))
+  for i in range(rows+1):
+    print(i*"*")
+
+
+
